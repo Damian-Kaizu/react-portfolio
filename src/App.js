15641800521be551cfa5f5a8ed4466191f0acbaa
Damian Kaizu <hyvinkaimeiro976@gmail.com>
2023-01-24 23:32:40 +0630

    Updated

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Home from "./components/home/Home";
 import "./App.css";
 import "./script";
 import { DarkModeProvider } from "./context/DarkModeContext";
+import About from "./components/about/About";
+import Skills from "./components/skills/Skills";
 
 const App = () => {
   return (
@@ -13,6 +15,8 @@ const App = () => {
         <Header />
         <main className="main">
           <Home />
+          <About />
+          <Skills />
         </main>
       </div>
     </DarkModeProvider>
